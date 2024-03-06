4260710e29afe51e2a82378df13bbaf54abe701a
legosfu <yuenlegolaswork@gmail.com>
2024-03-06 15:19:45 -0800

    init commit
    Initialization of spring boot project

---
+++ tech-connect-project/src/main/java/techconnect/techconnectproject/TechConnectProjectApplication.java
@@ -0,0 +1,13 @@
+package techconnect.techconnectproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechConnectProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechConnectProjectApplication.class, args);
+	}
+
+}
---
+++ tech-connect-project/src/test/java/techconnect/techconnectproject/TechConnectProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package techconnect.techconnectproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechConnectProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
