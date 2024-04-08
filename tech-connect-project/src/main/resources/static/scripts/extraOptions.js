8db41567aa5ab87b86fff66381585075bedd5a58
E1337 NOTORඞOUS The PRESTඞGඞOUS <45579671+ENTPRESTIGIOUS@users.noreply.github.com>
2024-04-07 23:30:35 -0700

    commence Extra Options

--- tech-connect-project/src/main/resources/static/scripts/extraOptions.js
+++ tech-connect-project/src/main/resources/static/scripts/extraOptions.js
@@ -1,5 +1,7 @@
 
-let x = document.getElementById("modeSwitch");
+let lightMode = document.getElementById("modeSwitchLight");
+let darkMode = document.getElementById("modeSwitchDark");
+let aboutProject = document.getElementById("aboutProject");
 // let extraOptions = document.getElementById("extraOptions");
 // let y = document.getElementsByClassName("fa-solid fa-lightbulb")[0];
 let dropdownMenuBtn = document.getElementById("dropdownMenuButton");
@@ -25,24 +27,42 @@ window.onload = function(){
     }
 }
 
-x.addEventListener('click', function() {
-    if(document.body.getAttribute('data-bs-theme') === 'light'){
-    // if(localStorage.getItem('theme') == 'light'){
-        document.body.setAttribute('data-bs-theme', 'dark')
-        dropdownMenuBtn.setAttribute('class', 'btn btn-light btn-sm position-absolute bottom-0 end-0')
-        // y.setAttribute('class', 'fa-regular fa-lightbulb')
-        // x.setAttribute('class', 'btn btn-light btn-lg position-absolute bottom-0 end-0')
-        // extraOptions.setAttribute('class', 'btn btn-dark btn-sm position-absolute bottom-0 start-0')
-        localStorage.setItem('theme', 'dark')
-        console.log('Theme switched to dark')
+lightMode.addEventListener('click', function() {
+    if (localStorage.getItem('theme') == 'light') {
+        return;
     }
-    else{
-        document.body.setAttribute('data-bs-theme', 'light')
-        dropdownMenuBtn.setAttribute('class', 'btn btn-dark btn-sm position-absolute bottom-0 end-0')
-        // y.setAttribute('class', 'fa-solid fa-lightbulb')
-        // x.setAttribute('class', 'btn btn-dark btn-lg position-absolute bottom-0 end-0')
-        // extraOptions.setAttribute('class', 'btn btn-light btn-sm position-absolute bottom-0 start-0')
-        console.log('Theme switched to light')
-        localStorage.setItem('theme', 'light')
+    document.body.setAttribute('data-bs-theme', 'light')
+    dropdownMenuBtn.setAttribute('class', 'btn btn-dark btn-sm position-absolute bottom-0 end-0')
+    console.log('Theme switched to light')
+    localStorage.setItem('theme', 'light')
+});
+
+darkMode.addEventListener('click', function() {
+    if (localStorage.getItem('theme') == 'dark') {
+        return;
     }
-})
+    document.body.setAttribute('data-bs-theme', 'dark')
+    dropdownMenuBtn.setAttribute('class', 'btn btn-light btn-sm position-absolute bottom-0 end-0')
+    localStorage.setItem('theme', 'dark')
+    console.log('Theme switched to dark')
+});
+
+aboutProject.addEventListener('click', function() {
+    console.log("Working...");
+    let aboutText = document.createElement('button');
+    aboutText.setAttribute('style', 'text-align: center; position: absolute; top: 10px; left: 0; right: 0; margin-left: auto; margin-right: auto; border-radius: 20px; width: 20%;');
+    // aboutText.setAttribute('class', 'reveal');
+    if (localStorage.getItem('theme') == 'dark') {
+        aboutText.setAttribute('class', 'btn btn-light btn-sm reveal');
+    }
+    else {
+        aboutText.setAttribute('class', 'btn btn-dark btn-sm reveal');
+    }
+    aboutText.innerHTML = `<i class = "fa-solid fa-circle-info"></i> Created by Team WLDMK Spring 2024`;
+    let appendToSite = document.getElementById('siteStats');
+    appendToSite.appendChild(aboutText);
+
+    setTimeout(function(){
+        appendToSite.removeChild(aboutText);
+    }, 3000);
+});
